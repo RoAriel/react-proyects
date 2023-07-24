aaef62c049c75f53ee4a1e4eec54beb995fee94e
rodrigo gonzalez <roariel06@gmail.com>
2023-07-24 12:29:05 -0300

    Primer acercamiento con estilos

---
+++ 01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+export function App (){
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/midudev/" alt="Avatar de Usuario" />
+                <div>
+                    <strong>Miguel Ángel Durán</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
