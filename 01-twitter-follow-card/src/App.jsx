90c34882161c377b4424b7614d2e67b789b29b8a
rodrigo gonzalez <roariel06@gmail.com>
2023-07-26 10:42:42 -0300

    Se crea el componente TwitterFollowCard y se estila con css

--- 01-twitter-follow-card/src/App.jsx
+++ 01-twitter-follow-card/src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 export function App (){
     return (
-        <article>
-            <header>
-                <img src="https://unavatar.io/midudev/" alt="Avatar de Usuario" />
-                <div>
-                    <strong>Miguel Ángel Durán</strong>
-                    <span>@midudev</span>
-                </div>
-            </header>
-            <aside>
-                <button>Seguir</button>
-            </aside>
-        </article>
+        <>
+            <div className='App'>
+                <TwitterFollowCard isFollowing userName='midudev'>
+                 Miguel Ángel Durán
+                </TwitterFollowCard>
+                
+                <TwitterFollowCard isFollowing={false} userName='MoureDev'>
+                 Brais Moure
+                </TwitterFollowCard>
+                
+                <TwitterFollowCard isFollowing userName='iamCarolinaSV'>
+                Carolina
+                </TwitterFollowCard>
+            </div>
+        </>
     )
 }
