8cb7d8c13329b9f48aa61b0d25e3bff2032986bb
rodrigo gonzalez <roariel06@gmail.com>
2023-08-04 17:40:25 -0300

    Win board

--- 02-ta-te-ti/src/App.jsx
+++ 02-ta-te-ti/src/App.jsx
@@ -18,19 +18,52 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+  const WINNER_COMBOS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,5,6],
+  ]
 
 function App() {
   const  [board, setBoar] = useState(Array(9).fill(null))
   const [turn, setTurn ] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // null not winner, false tie
+
+  const checkWinner = (boardToCheck) =>{
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+        ) { 
+          return boardToCheck [a]
+        }
+    }
+  }
   
   const updateBoard = (index) =>{
-  
+    
+    if(board[index] || winner) return
+
     const newBoard = [... board]
     newBoard[index] = turn
     setBoar(newBoard)
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+
+    if(newWinner){
+      setWinner(newWinner)
+
+    }
   }
 
   return (
